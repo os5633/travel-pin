fccd1afdb57211a631e89a4e79b4720bc8d824c3
ian <osung5633@gmail.com>
2022-12-05 23:47:20 +0900

    fix:firebase세팅 수정

--- lib/main.dart
+++ lib/main.dart
@@ -5,11 +5,12 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:travelpin/app/app.dart';
+import 'package:travelpin/firebase_options.dart';
 import 'package:travelpin/repository/repository.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   Bloc.observer = AppBlocObserver();
   final authenticationRepository = AuthenticationRepository();
   FirebaseAuth.instance.signInAnonymously();
